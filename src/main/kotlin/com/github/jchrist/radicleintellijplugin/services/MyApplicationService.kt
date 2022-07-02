2c298cf8798b0eee3cdfa3ea913cd1443da1534d
GitHub Action <action@github.com>
2022-07-02 09:21:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jchrist/radicleintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jchrist.radicleintellijplugin.services
+
+import com.github.jchrist.radicleintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/jchrist/radicleintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jchrist/radicleintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jchrist.radicleintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jchrist.radicleintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
