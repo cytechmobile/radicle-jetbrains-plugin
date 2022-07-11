daf5fed3376cbc68023a00a575c50ca4dc605332
Yorgos Saslis <yorgo@protonmail.com>
2022-07-11 16:54:24 +0300

    Introduces Logging
    as per https://plugins.jetbrains.com/docs/intellij/ide-infrastructure.html#logging
    
    this fixes some Qodana warnings about use of System.out and System.err
    
    Signed-off-by: Yorgos Saslis <yorgo@protonmail.com>

--- src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/services/RadicleApplicationService.java
+++ src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/services/RadicleApplicationService.java
@@ -1,10 +1,13 @@
 package network.radicle.jetbrains.radiclejetbrainsplugin.services;
 
+import com.intellij.openapi.diagnostic.Logger;
 import network.radicle.jetbrains.radiclejetbrainsplugin.RadicleBundle;
 
 public class RadicleApplicationService {
 
+    private static final Logger log = Logger.getInstance(RadicleApplicationService.class);
+
     public RadicleApplicationService() {
-        System.out.println(RadicleBundle.message("applicationService"));
+        log.info(RadicleBundle.message("applicationService"));
     }
 }
