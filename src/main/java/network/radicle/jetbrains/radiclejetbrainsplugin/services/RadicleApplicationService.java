1ff18fe755f228bc0ea0eeebba6cd087b4508c58
Stelios Mavrommatakis <steliosmavr@cytech.gr>
2022-08-05 18:35:42 +0300

    check if seed node is configured before run an event

--- src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/services/RadicleApplicationService.java
+++ src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/services/RadicleApplicationService.java
@@ -9,6 +9,7 @@ import com.intellij.openapi.util.SystemInfo;
 import com.intellij.serviceContainer.NonInjectable;
 import git4idea.repo.GitRepository;
 import git4idea.util.GitVcsConsoleWriter;
+import network.radicle.jetbrains.radiclejetbrainsplugin.RadicleBundle;
 import network.radicle.jetbrains.radiclejetbrainsplugin.config.RadicleSettingsHandler;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
@@ -16,6 +17,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Objects;
 
@@ -58,16 +61,37 @@ public class RadicleApplicationService {
                 Objects.requireNonNull(root.getCurrentBranchName())), root);
     }
 
-
     public ProcessOutput executeCommand(String workDir, List<String> args, @Nullable GitRepository repo) {
         final var settings = settingsHandler.loadSettings();
         final var radPath = settings.getPath();
         return executeCommand(radPath, workDir, args, repo);
     }
 
+    private boolean isSeedNodeConfigured(String workDir) {
+        var seedNodes = List.of("pine.radicle.garden","willow.radicle.garden","mapple.radicle.garden");
+        boolean hasSeedNode = false;
+        final String gitConfigFile = "/.git/config";
+        Path filePath = Path.of(workDir + gitConfigFile);
+        try {
+            String content = Files.readString(filePath);
+            for (String node : seedNodes) {
+                hasSeedNode = content.contains(node);
+                if (hasSeedNode) break;
+            }
+        } catch (Exception e) {
+            logger.warn("unable to read git config file",e);
+        }
+        return hasSeedNode;
+    }
+
     public ProcessOutput executeCommand(
             String radPath, String workDir, List<String> args, @Nullable GitRepository repo) {
         final var cmdLine = new GeneralCommandLine();
+        if (!isSeedNodeConfigured(workDir)) {
+            var po = new ProcessOutput(-1);
+            po.appendStderr(RadicleBundle.message("seedNodeMissing"));
+            return po;
+        }
         if (SystemInfo.isWindows) {
             //TODO remove wsl
             cmdLine.withExePath("wsl").withParameters(radPath);
