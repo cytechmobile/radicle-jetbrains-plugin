bf9c1f5db4614d9f6b25768ec2a5a5a1719bd5d1
Stelios Mavrommatakis <steliosmavr@cytech.gr>
2022-10-19 19:16:02 +0300

    fix a minor bug in clone

--- src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/actions/RadicleCloneAction.java
+++ src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/actions/RadicleCloneAction.java
@@ -2,6 +2,8 @@ package network.radicle.jetbrains.radiclejetbrainsplugin.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.vcs.CheckoutProvider;
+import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.util.ui.cloneDialog.VcsCloneDialog;
 import network.radicle.jetbrains.radiclejetbrainsplugin.dialog.clone.RadCheckoutProvider;
 import org.jetbrains.annotations.NotNull;
@@ -11,7 +13,15 @@ import java.util.Objects;
 public class RadicleCloneAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
+        var project = e.getProject();
+        if (project == null) {
+            return ;
+        }
+        CheckoutProvider.Listener checkoutListener = ProjectLevelVcsManager.getInstance(project).getCompositeCheckoutListener();
         VcsCloneDialog dialog = new VcsCloneDialog.Builder(Objects.requireNonNull(e.getProject())).forVcs(RadCheckoutProvider.class);
-        dialog.showAndGet();
+        if (dialog.showAndGet()) {
+            dialog.doClone(checkoutListener);
+        }
     }
 }
+
