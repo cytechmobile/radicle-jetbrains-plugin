ca2f0fd8f0bb67ddede9d1903e14fcca4091058d
Stelios Mavrommatakis <steliosmavr@cytech.gr>
2023-01-12 16:15:34 +0200

    Make icons final

--- src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/icons/RadicleIcons.java
+++ src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/icons/RadicleIcons.java
@@ -8,21 +8,21 @@ import javax.swing.Icon;
 public class  RadicleIcons {
     private static final String ICON_PATH = "/icons/";
 
-    public static Icon RADICLE = getIcon("radicle", false);
+    public static final Icon RADICLE = getIcon("radicle", false);
 
-    public static Icon RADICLE_TOOL_WINDOW = getIcon("rad_tool_window", true);
+    public static final Icon RADICLE_TOOL_WINDOW = getIcon("rad_tool_window", true);
 
-    public static Icon RADICLE_PUSH = getIcon("rad_push", true);
+    public static final Icon RADICLE_PUSH = getIcon("rad_push", true);
 
-    public static Icon RADICLE_PULL = getIcon("rad_pull", true);
+    public static final Icon RADICLE_PULL = getIcon("rad_pull", true);
 
-    public static Icon RADICLE_SYNC = getIcon("rad_sync", true);
+    public static final Icon RADICLE_SYNC = getIcon("rad_sync", true);
 
-    public static Icon RADICLE_CLONE = getIcon("rad_clone", true);
+    public static final Icon RADICLE_CLONE = getIcon("rad_clone", true);
 
-    public static Icon RADICLE_SHARE = getIcon("rad_share", true);
+    public static final Icon RADICLE_SHARE = getIcon("rad_share", true);
 
-    public static Icon CHECK_ICON = getIcon("check", false);
+    public static final Icon CHECK_ICON = getIcon("check", false);
 
     private static boolean isNewUiEnabled() {
         return Registry.get("ide.experimental.ui").asBoolean();
