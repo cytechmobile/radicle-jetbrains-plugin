e27b6aece3076e5eae8564ecf28600fa35c7ee77
Stelios Mavrommatakis <steliosmavr@cytech.gr>
2023-01-26 12:28:29 +0200

    Replace deprecated method projectOpened with runActivity
    Signed-off-by: Stelios Mavrommatakis <steliosmavr@cytech.gr>

--- src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/listeners/RadicleManagerListener.java
+++ src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/listeners/RadicleManagerListener.java
@@ -4,23 +4,17 @@ import com.google.common.base.Strings;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManagerListener;
+import com.intellij.openapi.startup.StartupActivity;
 import network.radicle.jetbrains.radiclejetbrainsplugin.RadicleBundle;
 import network.radicle.jetbrains.radiclejetbrainsplugin.actions.rad.RadAction;
 import network.radicle.jetbrains.radiclejetbrainsplugin.config.RadicleSettingsHandler;
 import network.radicle.jetbrains.radiclejetbrainsplugin.services.RadicleProjectService;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public class RadicleManagerListener implements ProjectManagerListener {
+public class RadicleManagerListener implements StartupActivity {
     private static final Logger log = Logger.getInstance(RadicleManagerListener.class);
-
-    @Override
-    public void projectOpened(Project project) {
-        var srv = project.getService(RadicleProjectService.class);
-        showSuccessNotification(project);
-        log.info("got service: " + srv);
-    }
 
     private void showSuccessNotification(Project project) {
         var radicleSettingsHandler = new RadicleSettingsHandler();
@@ -29,5 +23,12 @@ public class RadicleManagerListener implements ProjectManagerListener {
             RadAction.showNotification(project, "radicle", "installedSuccessfully", NotificationType.INFORMATION,
                     List.of(new RadAction.ConfigureRadCliNotificationAction(project, RadicleBundle.lazyMessage("configure"))));
         }
+    }
+
+    @Override
+    public void runActivity(@NotNull Project project) {
+        var srv = project.getService(RadicleProjectService.class);
+        showSuccessNotification(project);
+        log.info("got service: " + srv);
     }
 }
