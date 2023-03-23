c6ff630a0e639455ab3f8afbb73f53676ae79d4d
Stelios Mavrommatakis <steliosmavr@cytech.gr>
2023-03-23 11:15:42 +0200

    Remove getProfiles / activeProfile. We want only to run rad self in this case to get the node / identity details
    Signed-off-by: Stelios Mavrommatakis <steliosmavr@cytech.gr>

--- src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/actions/rad/RadSelf.java
+++ src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/actions/rad/RadSelf.java
@@ -1,64 +1,31 @@
 package network.radicle.jetbrains.radiclejetbrainsplugin.actions.rad;
 
-import com.google.common.base.Strings;
 import com.intellij.execution.process.ProcessOutput;
 import com.intellij.openapi.application.ApplicationManager;
 import network.radicle.jetbrains.radiclejetbrainsplugin.services.RadicleApplicationService;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class RadSelf extends RadAction {
-    private RadSelfAction action;
+    private final String radHome;
+    private final String radPath;
 
-    public RadSelf(RadSelfAction action) {
-        this.action = action;
-    }
-
-    @Override
-    public String getActionName() {
-        if (action == RadSelfAction.ACTIVE_PROFILE) {
-            return "SelfActiveProfile";
-        } else if (action == RadSelfAction.GET_PROFILES) {
-            return "SelfGetProfiles";
-        }
-        return "";
-    }
-
-    public ProcessOutput activeProfile() {
-        var rad = ApplicationManager.getApplication().getService(RadicleApplicationService.class);
-        return rad.self(true);
-    }
-
-    private ProcessOutput getProfiles() {
-        var storage = getStoragePath();
-        if (storage == null || Strings.isNullOrEmpty(storage.keysStoragePath)) {
-            return new ProcessOutput(-1);
-        }
-        var rad = ApplicationManager.getApplication().getService(RadicleApplicationService.class);
-        return rad.executeCommand("ls", ".", List.of(storage.keysStoragePath), null, false);
+    public RadSelf(String radHome, String radPath) {
+        this.radHome = radHome;
+        this.radPath = radPath;
     }
 
     @Override
     public ProcessOutput run() {
-        switch (action) {
-            case ACTIVE_PROFILE:
-                return activeProfile();
-            case GET_PROFILES:
-                var output = getProfiles();
-                if (output.getExitCode() == 0) {
-                    var profiles = output.getStdoutLines(true).stream().filter(p -> p.contains("-"))
-                            .collect(Collectors.toList());
-                    output = new ProcessOutput(0);
-                    output.appendStdout(String.join(",", profiles));
-                }
-                return output;
-            default:
-                return new ProcessOutput(-1);
-        }
+        var rad = ApplicationManager.getApplication().getService(RadicleApplicationService.class);
+        return rad.self(radHome, radPath);
     }
 
-    public enum RadSelfAction {
-        ACTIVE_PROFILE, GET_PROFILES
+    @Override
+    public boolean showNotification() {
+        return false;
+    }
+
+    @Override
+    public String getActionName() {
+        return "Self";
     }
 }
