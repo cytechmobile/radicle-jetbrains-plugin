cdc65b496914ad1399fca3d29f22e40d99fdbd5a
Stelios Mavrommatakis <steliosmavr@cytech.gr>
2023-03-28 16:49:20 +0300

    Fix the tests to assert for `rad fetch` instead of `rad sync`. Also remove unnecessary assertions
    Signed-off-by: Stelios Mavrommatakis <steliosmavr@cytech.gr>

--- src/test/java/network/radicle/jetbrains/radiclejetbrainsplugin/config/RadicleSettingsHandlerTest.java
+++ src/test/java/network/radicle/jetbrains/radiclejetbrainsplugin/config/RadicleSettingsHandlerTest.java
@@ -18,13 +18,11 @@ public class RadicleSettingsHandlerTest extends LightPlatform4TestCase {
     public void before() {
         radicleSettingsHandler = new RadicleSettingsHandler();
         radicleSettingsHandler.savePath(null);
-        radicleSettingsHandler.saveRadSync(RadicleSettings.RadSyncType.ASK);
     }
 
     @After
     public void after() {
         radicleSettingsHandler.savePath(null);
-        radicleSettingsHandler.saveRadSync(RadicleSettings.RadSyncType.ASK);
     }
 
     @Test
@@ -32,18 +30,12 @@ public class RadicleSettingsHandlerTest extends LightPlatform4TestCase {
         var settings = radicleSettingsHandler.loadSettings();
         var newSeedNodes = List.of(new SeedNode("192.168.1.1", "8080"));
         assertThat(settings.getPath()).isEmpty();
-        assertThat(settings.getRadSync()).isEqualTo(RadicleSettings.RadSyncType.ASK.val);
         assertThat(settings.getSeedNodes()).usingRecursiveComparison().isEqualTo(RadicleSettingsHandler.DEFAULT_SEED_NODES);
         radicleSettingsHandler.savePath(AbstractIT.RAD_PATH);
-        radicleSettingsHandler.saveRadSync(RadicleSettings.RadSyncType.YES);
         radicleSettingsHandler.saveSeedNodes(newSeedNodes);
         var newSettings = radicleSettingsHandler.loadSettings();
         assertThat(newSettings.getPath()).isEqualTo(AbstractIT.RAD_PATH);
-        assertThat(newSettings.getRadSync()).isEqualTo(RadicleSettings.RadSyncType.YES.val);
         assertThat(newSettings.getSeedNodes()).usingRecursiveComparison().isEqualTo(newSeedNodes);
-        radicleSettingsHandler.saveRadSync(RadicleSettings.RadSyncType.NO);
-        newSettings = radicleSettingsHandler.loadSettings();
-        assertThat(newSettings.getRadSync()).isEqualTo(RadicleSettings.RadSyncType.NO.val);
         radicleSettingsHandler.saveSeedNodes(null);
     }
 
