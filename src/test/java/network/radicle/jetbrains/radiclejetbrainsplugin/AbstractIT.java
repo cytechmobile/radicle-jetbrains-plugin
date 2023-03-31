c47a9d70b89f0c2d43fb222b0b8b63d880c31b13
Stelios Mavrommatakis <steliosmavr@cytech.gr>
2023-03-31 13:38:24 +0300

    Remove unused import
    Signed-off-by: Stelios Mavrommatakis <steliosmavr@cytech.gr>

--- src/test/java/network/radicle/jetbrains/radiclejetbrainsplugin/AbstractIT.java
+++ src/test/java/network/radicle/jetbrains/radiclejetbrainsplugin/AbstractIT.java
@@ -5,7 +5,6 @@ import com.intellij.notification.Notification;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.changes.Change;
