89fba47172eabd339b026e6b608ada16d6fa84d0
Stelios Mavrommatakis <steliosmavr@cytech.gr>
2023-03-31 18:16:06 +0300

    Get the node api url from an object instead of a list
    Signed-off-by: Stelios Mavrommatakis <steliosmavr@cytech.gr>

--- src/test/java/network/radicle/jetbrains/radiclejetbrainsplugin/config/RadicleSettingsHandlerTest.java
+++ src/test/java/network/radicle/jetbrains/radiclejetbrainsplugin/config/RadicleSettingsHandlerTest.java
@@ -2,12 +2,9 @@ package network.radicle.jetbrains.radiclejetbrainsplugin.config;
 
 import com.intellij.testFramework.LightPlatform4TestCase;
 import network.radicle.jetbrains.radiclejetbrainsplugin.AbstractIT;
-import network.radicle.jetbrains.radiclejetbrainsplugin.models.SeedNode;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,16 +25,15 @@ public class RadicleSettingsHandlerTest extends LightPlatform4TestCase {
     @Test
     public void loadSettings() {
         var settings = radicleSettingsHandler.loadSettings();
-        var newSeedNodes = List.of(new SeedNode("http://127.0.0.1:8080"));
+        var newSeedNode = "http://127.0.0.1:8080";
         assertThat(settings.getPath()).isEmpty();
-        assertThat(settings.getSeedNodes()).usingRecursiveComparison().isEqualTo(RadicleSettingsHandler.DEFAULT_SEED_NODES);
+        assertThat(settings.getSeedNode().url).isEqualTo(RadicleSettingsHandler.DEFAULT_SEED_NODES);
         radicleSettingsHandler.savePath(AbstractIT.RAD_PATH);
-        radicleSettingsHandler.saveSeedNodes(newSeedNodes);
+        radicleSettingsHandler.saveSeedNode(newSeedNode);
         var newSettings = radicleSettingsHandler.loadSettings();
         assertThat(newSettings.getPath()).isEqualTo(AbstractIT.RAD_PATH);
-        assertThat(newSettings.getSeedNodes()).usingRecursiveComparison().isEqualTo(newSeedNodes);
-        newSettings = radicleSettingsHandler.loadSettings();
-        radicleSettingsHandler.saveSeedNodes(null);
+        assertThat(newSettings.getSeedNode().url).isEqualTo(newSeedNode);
+        radicleSettingsHandler.saveSeedNode(null);
     }
 
 }
