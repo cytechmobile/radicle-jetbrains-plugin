a60f592f88dc875b783f04b242ee0284d79690aa
Stelios Mavrommatakis <steliosmavr@cytech.gr>
2023-04-26 18:03:40 +0300

    Change the library
    Signed-off-by: Stelios Mavrommatakis <steliosmavr@cytech.gr>

--- src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/models/SeedNode.java
+++ src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/models/SeedNode.java
@@ -3,7 +3,7 @@ package network.radicle.jetbrains.radiclejetbrainsplugin.models;
 public class SeedNode {
     public String url;
 
-    public SeedNode(String url) {
+    public  SeedNode(String url) {
         this.url = url;
     }
 
