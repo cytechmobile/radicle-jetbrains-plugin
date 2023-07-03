ebe806e697c59d8f6abf226bf90e0b3fc74107ee
Stelios Mavrommatakis <steliosmavr@cytech.gr>
2023-07-03 11:45:57 +0300

    Refactor issue & patch filters. Move calculateCommits function inside RadPatch class.
    Signed-off-by: Stelios Mavrommatakis <steliosmavr@cytech.gr>

--- src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/issues/IssueSearchPanelViewModel.java
+++ src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/issues/IssueSearchPanelViewModel.java
@@ -3,33 +3,24 @@ package network.radicle.jetbrains.radiclejetbrainsplugin.issues;
 import com.intellij.collaboration.ui.codereview.list.search.ReviewListQuickFilter;
 import com.intellij.collaboration.ui.codereview.list.search.ReviewListSearchHistoryModel;
 import com.intellij.openapi.project.Project;
-import git4idea.repo.GitRepositoryManager;
 import kotlin.jvm.functions.Function2;
 import kotlinx.coroutines.CoroutineScope;
 import kotlinx.coroutines.flow.MutableStateFlow;
-import network.radicle.jetbrains.radiclejetbrainsplugin.actions.rad.RadAction;
+import network.radicle.jetbrains.radiclejetbrainsplugin.models.RadAuthor;
 import network.radicle.jetbrains.radiclejetbrainsplugin.models.RadIssue;
 import network.radicle.jetbrains.radiclejetbrainsplugin.toolwindow.SearchViewModelBase;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 public class IssueSearchPanelViewModel extends SearchViewModelBase<IssueListSearchValue, IssueSearchPanelViewModel.IssueListQuickFilter, RadIssue> {
-    private static final Logger logger = LoggerFactory.getLogger(IssueSearchPanelViewModel.class);
-
-    private final Project project;
-    private List<String> projectNames = List.of();
     public IssueSearchPanelViewModel(@NotNull CoroutineScope scope,
                                      @NotNull ReviewListSearchHistoryModel<IssueListSearchValue> historyModel,
                                      Project project) {
-        super(scope, historyModel, new IssueListSearchValue(), new IssueListQuickFilter());
-        this.project = project;
+        super(scope, historyModel, new IssueListSearchValue(), new IssueListQuickFilter(), project);
     }
 
     public MutableStateFlow<String> authorFilterState() {
@@ -77,39 +68,12 @@ public class IssueSearchPanelViewModel extends SearchViewModelBase<IssueListSear
                 });
     }
 
-    public CompletableFuture<List<String>> getProjectNames() {
-        return CompletableFuture.supplyAsync(() -> {
-            var gitRepoManager = GitRepositoryManager.getInstance(project);
-            projectNames = RadAction.getInitializedReposWithNodeConfigured(gitRepoManager.getRepositories(), true)
-                    .stream().map(e -> e.getRoot().getName()).collect(Collectors.toList());
-            return projectNames;
-        });
-    }
-
-    public CompletableFuture<List<String>> getTags() {
-        return CompletableFuture.supplyAsync(() -> {
-            List<String> tags = new ArrayList<>();
-            var selectedProjectFilter = this.getSearchState().getValue().project;
-            for (var issue : myList) {
-                if (selectedProjectFilter != null && !issue.repo.getRoot().getName().equals(selectedProjectFilter)) {
-                    continue;
-                }
-                for (var tag : issue.tags) {
-                    if (!tags.contains(tag)) {
-                        tags.add(tag);
-                    }
-                }
-            }
-            return tags;
-        });
-    }
-
     public CompletableFuture<List<String>> getAssignees() {
         return CompletableFuture.supplyAsync(() -> {
             List<String> assigness = new ArrayList<>();
-            var selectedProjectFilter = this.getSearchState().getValue().project;
+            var selectedProjectFilter = getSelectedProjectFilter();
             for (var issue : myList) {
-                if (selectedProjectFilter != null && !issue.repo.getRoot().getName().equals(selectedProjectFilter)) {
+                if (selectedProjectFilter != null && isProjectFilterMatch(issue, selectedProjectFilter)) {
                     continue;
                 }
                 for (var assignee : issue.assignees) {
@@ -122,20 +86,24 @@ public class IssueSearchPanelViewModel extends SearchViewModelBase<IssueListSear
         });
     }
 
-    public CompletableFuture<List<String>> getAuthors() {
-        return CompletableFuture.supplyAsync(() -> {
-            List<String> peersIds = new ArrayList<>();
-            var selectedProjectFilter = this.getSearchState().getValue().project;
-            for (var issue : myList) {
-                if (selectedProjectFilter != null && !issue.repo.getRoot().getName().equals(selectedProjectFilter)) {
-                    continue;
-                }
-                if (!peersIds.contains(issue.author.id)) {
-                    peersIds.add(issue.author.id);
-                }
-            }
-            return peersIds;
-        });
+    @Override
+    protected String getSelectedProjectFilter() {
+        return this.getSearchState().getValue().project;
+    }
+
+    @Override
+    protected List<String> getItemTags(RadIssue issue) {
+        return issue.tags;
+    }
+
+    @Override
+    protected boolean isProjectFilterMatch(RadIssue issue, String projectFilter) {
+        return !issue.repo.getRoot().getName().equals(projectFilter);
+    }
+
+    @Override
+    protected RadAuthor getAuthor(RadIssue item) {
+        return item.author;
     }
 
     @NotNull
--- src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/toolwindow/SearchViewModelBase.java
+++ src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/toolwindow/SearchViewModelBase.java
@@ -4,21 +4,30 @@ import com.intellij.collaboration.ui.codereview.list.search.ReviewListQuickFilte
 import com.intellij.collaboration.ui.codereview.list.search.ReviewListSearchHistoryModel;
 import com.intellij.collaboration.ui.codereview.list.search.ReviewListSearchPanelViewModelBase;
 import com.intellij.collaboration.ui.codereview.list.search.ReviewListSearchValue;
+import com.intellij.openapi.project.Project;
+import git4idea.repo.GitRepositoryManager;
 import kotlinx.coroutines.CoroutineScope;
+import network.radicle.jetbrains.radiclejetbrainsplugin.actions.rad.RadAction;
+import network.radicle.jetbrains.radiclejetbrainsplugin.models.RadAuthor;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
+import java.util.stream.Collectors;
 
 public abstract class SearchViewModelBase<T extends ReviewListSearchValue, E extends ReviewListQuickFilter<T>, Q>
         extends ReviewListSearchPanelViewModelBase<T, E> {
-
+    protected final Project project;
     protected CountDownLatch countDown;
     protected List<Q> myList;
+    private List<String> projectNames = List.of();
 
     public SearchViewModelBase(@NotNull CoroutineScope scope, @NotNull ReviewListSearchHistoryModel<T> historyModel,
-                               @NotNull T emptySearch, @NotNull E defaultQuickFilter) {
+                               @NotNull T emptySearch, @NotNull E defaultQuickFilter, Project project) {
         super(scope, historyModel, emptySearch, defaultQuickFilter);
+        this.project = project;
     }
 
     public void setList(List<Q> list) {
@@ -36,4 +45,58 @@ public abstract class SearchViewModelBase<T extends ReviewListSearchValue, E ext
     public ReviewListSearchValue getValue() {
         return this.getSearchState().getValue();
     }
+
+    public CompletableFuture<List<String>> getProjectNames() {
+        return CompletableFuture.supplyAsync(() -> {
+            var gitRepoManager = GitRepositoryManager.getInstance(project);
+            projectNames = RadAction.getInitializedReposWithNodeConfigured(gitRepoManager.getRepositories(), true)
+                    .stream().map(e -> e.getRoot().getName()).collect(Collectors.toList());
+            return projectNames;
+        });
+    }
+
+    public CompletableFuture<List<String>> getTags() {
+        return CompletableFuture.supplyAsync(() -> {
+            List<String> tags = new ArrayList<>();
+            var selectedProjectFilter = getSelectedProjectFilter();
+            for (var el : myList) {
+                if (selectedProjectFilter != null && isProjectFilterMatch(el, selectedProjectFilter)) {
+                    continue;
+                }
+                List<String> itemTags = getItemTags(el);
+                for (var tag : itemTags) {
+                    if (!tags.contains(tag)) {
+                        tags.add(tag);
+                    }
+                }
+            }
+            return tags;
+        });
+    }
+
+    public CompletableFuture<List<String>> getAuthors() {
+        return CompletableFuture.supplyAsync(() -> {
+            List<String> peersIds = new ArrayList<>();
+            var selectedProjectFilter = getSelectedProjectFilter();
+            for (var el : myList) {
+                if (selectedProjectFilter != null && isProjectFilterMatch(el, selectedProjectFilter)) {
+                    continue;
+                }
+                var author = getAuthor(el);
+                if (!peersIds.contains(author.id)) {
+                    peersIds.add(author.id);
+                }
+            }
+            return peersIds;
+        });
+    }
+
+    protected abstract RadAuthor getAuthor(Q item);
+
+    protected abstract String getSelectedProjectFilter();
+
+    protected abstract List<String> getItemTags(Q item);
+
+    protected abstract boolean isProjectFilterMatch(Q item, String projectFilter);
+
 }
