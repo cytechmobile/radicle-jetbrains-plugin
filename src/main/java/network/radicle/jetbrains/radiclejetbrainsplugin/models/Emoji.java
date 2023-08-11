602406ed0b4a04b1951e7578beeec032500c9977
Stelios Mavrommatakis <steliosmavr@cytech.gr>
2023-08-11 13:32:26 +0300

    Add the emojis that GitHub has
    Signed-off-by: Stelios Mavrommatakis <steliosmavr@cytech.gr>

--- src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/models/Emoji.java
+++ src/main/java/network/radicle/jetbrains/radiclejetbrainsplugin/models/Emoji.java
@@ -21,10 +21,14 @@ public class Emoji {
 
     public static List<Emoji> loadEmojis() {
         return List.of(
-                new Emoji("Smiley Face", "\uD83D\uDE00"), // 😀
-                new Emoji("Heart", "\u2764"), // ❤
                 new Emoji("Thumbs Up", "\uD83D\uDC4D"), // 👍
-                new Emoji("Crying Face", "\uD83D\uDE22") // 😢
+                new Emoji("Thumbs Down", "\uD83D\uDC4E"), // 👎
+                new Emoji("Smiley Face", "\uD83D\uDE04"), // 😄
+                new Emoji("Tada", "\uD83C\uDF89"), // 🎉
+                new Emoji("Thinking Face", "\uD83D\uDE15"), // 😕
+                new Emoji("Heart", "\u2764"), // ❤
+                new Emoji("Rocket", "\uD83D\uDE80"), // 🚀
+                new Emoji("Eyes", "\uD83D\uDC40") // 👀
         );
     }
 }
