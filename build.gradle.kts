2141ce42685f1ba245ab1693ae69ae59bf34943d
stelios maurommatakis <steliosmavr@cytech.gr>
2024-05-20 13:26:22 +0300

    migrate to gradle version 2
    Signed-off-by: stelios maurommatakis <steliosmavr@cytech.gr>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,48 @@
 import org.jetbrains.changelog.Changelog
+import org.jetbrains.changelog.date
 import org.jetbrains.changelog.markdownToHTML
-
-fun properties(key: String) = providers.gradleProperty(key)
-fun environment(key: String) = providers.environmentVariable(key)
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+import org.jetbrains.intellij.platform.gradle.models.ProductRelease
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    // Java support
     id("java")
     checkstyle
     alias(libs.plugins.kotlin) // Kotlin support
-    alias(libs.plugins.gradleIntelliJPlugin) // Gradle IntelliJ Plugin
+    id("org.jetbrains.intellij.platform") version "2.0.0-beta2"
+    id("org.jetbrains.intellij.platform.migration") version "2.0.0-beta2"
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
 }
 
-group = properties("pluginGroup").get()
-version = properties("pluginVersion").get()
+group = providers.gradleProperty("pluginGroup")
+version = providers.gradleProperty("pluginVersion")
 
-// Configure project's dependencies
 repositories {
     mavenCentral()
     maven {
         url = uri("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
     }
+    intellijPlatform {
+        defaultRepositories()
+        jetbrainsRuntime()
+        intellijDependencies()
+    }
 }
 
-var remoteRobotVersion: String = libs.versions.remoteRobot.get();
 dependencies {
-    testImplementation("com.intellij.remoterobot:remote-robot:$remoteRobotVersion")
-    testImplementation("com.intellij.remoterobot:remote-fixtures:$remoteRobotVersion")
+    intellijPlatform {
+        create(
+            providers.gradleProperty("platformType"),
+            providers.gradleProperty("platformVersion")
+        )
+        bundledPlugins(providers.gradleProperty("platformPlugins").map { it.split(',') }.getOrElse(emptyList()))
+         instrumentationTools()
+        // pluginVerifier()
+        // zipSigner()
+    }
+
+    // testImplementation("com.intellij.remoterobot:remote-robot:$remoteRobotVersion")
+    // testImplementation("com.intellij.remoterobot:remote-fixtures:$remoteRobotVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:${libs.versions.junitJupiter.get()}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${libs.versions.junitJupiter.get()}")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:${libs.versions.junitJupiter.get()}")
@@ -46,150 +61,160 @@ dependencies {
     implementation("com.sshtools:maverick-synergy-client:${libs.versions.sshTools.get()}")
 }
 
-checkstyle {
-//    configFile = File("./checkstyle.xml");
-    // we do not tolerate errors or warnings, break build if any found
-    toolVersion = libs.versions.checkstyleTools.get();
-    maxErrors = 0;
-    maxWarnings = 0;
-}
-
-// Set the JVM language level used to build the project.
-kotlin {
-    jvmToolchain(17)
-}
-
-// Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-intellij {
-    pluginName = properties("pluginName")
-    version = properties("platformVersion")
-    type = properties("platformType")
-
-    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
-}
-
-// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-changelog {
-//    version.set(properties("pluginVersion"))
-    groups.empty()
-    repositoryUrl = properties("pluginRepositoryUrl")
-}
-
-tasks {
-    // Set the JVM compatibility versions
-    properties("javaVersion").get().let {
-        withType<JavaCompile> {
-            sourceCompatibility = it
-            targetCompatibility = it
-        }
-    }
-
-    wrapper {
-        gradleVersion = properties("gradleVersion").get()
-    }
-
-    patchPluginXml {
-        version = properties("pluginVersion")
-        sinceBuild = properties("pluginSinceBuild")
-        untilBuild = properties("pluginUntilBuild")
+// Read more: https://github.com/JetBrains/gradle-intellij-plugin
+intellijPlatform {
+    pluginConfiguration {
+        id = providers.gradleProperty("pluginId")
+        name = providers.gradleProperty("pluginName")
+        version = providers.gradleProperty("pluginVersion")
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        pluginDescription = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
-            val start = "<!-- Plugin description -->"
-            val end = "<!-- Plugin description end -->"
-            with(it.lines()) {
+        description = providers.fileContents(layout.projectDirectory.file("./README.md")).asText.map {
+            it.lines().run {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
+
                 if (!containsAll(listOf(start, end))) {
                     throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
                 }
-                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
-            }
+                subList(indexOf(start) + 1, indexOf(end))
+            }.joinToString("\n")
+        }.map {
+            markdownToHTML(it)
         }
 
-        val changelog = project.changelog // local variable for configuration cache compatibility
         // Get the latest available change notes from the changelog file
-        changeNotes = properties("pluginVersion").map { pluginVersion ->
-            with(changelog) {
-                renderItem(
-                    (getOrNull(pluginVersion) ?: getUnreleased())
-                        .withHeader(false)
-                        .withEmptySections(false),
-                    Changelog.OutputType.HTML,
-                )
-            }
+        changeNotes.set(provider {
+            changelog.renderItem(
+                changelog.getLatest(),
+                Changelog.OutputType.HTML
+            )
+        })
+
+        ideaVersion {
+            sinceBuild = providers.gradleProperty("pluginSinceBuild")
+            untilBuild = provider { null } // removes until-build in plugin.xml
+        }
+
+        vendor {
+            name = providers.gradleProperty("pluginVendor")
+            url = providers.gradleProperty("pluginVendorUrl")
         }
     }
 
-    downloadRobotServerPlugin {
-        version = remoteRobotVersion
-    }
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
 
-    runIde {
-        systemProperty("idea.is.internal", "true");
-    }
-
-    // Configure UI tests plugin
-    // Read more: https://github.com/JetBrains/intellij-ui-test-robot
-    runIdeForUiTests {
-        //    In case your Idea is launched on remote machine you can enable public port and enable encryption of JS calls
-        //    systemProperty "robot-server.host.public", "true"
-        //    systemProperty "robot.encryption.enabled", "true"
-        //    systemProperty "robot.encryption.password", "my super secret"
-
-        systemProperty("robot-server.port", "8082")
-        systemProperty("ide.mac.message.dialogs.as.sheets", "false")
-        systemProperty("jb.privacy.policy.text", "<!--999.999-->")
-        systemProperty("jb.consents.confirmation.enabled", "false")
-        systemProperty("ide.mac.file.chooser.native", "false")
-        systemProperty("apple.laf.useScreenMenuBar", "false")
-        systemProperty("jbScreenMenuBar.enabled", "false")
-        systemProperty("idea.trust.all.projects", "true")
-        systemProperty("ide.show.tips.on.startup.default.value", "false")
-        //    systemProperty "eap.require.license", "true"
-
-//        val projectPath = environment("PROJECT_PATH").getOrElse("")
-//        if (projectPath.isNotEmpty()) {
-//            args (projectPath)
-//        }
-
-    }
-
-    test {
-        // enable here nad in runIdeForUiTests block - to log the retrofit HTTP calls
-        // systemProperty "debug-retrofit", "enable"
-
-        // enable encryption on test side when use remote machine
-        // systemProperty "robot.encryption.password", "my super secret"
-        exclude("network/radicle/jetbrains/radiclejetbrainsplugin/remoterobot/**")
-    }
-
-
-    signPlugin {
-        certificateChain = environment("CERTIFICATE_CHAIN")
-        privateKey = environment("PRIVATE_KEY")
-        password = environment("PRIVATE_KEY_PASSWORD")
-    }
-
-    publishPlugin {
-        dependsOn("patchChangelog")
-        token = environment("PUBLISH_TOKEN")
         // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        // channels = properties("pluginVersion").map { listOf(it.split('-').getOrElse(1) { "default" }.split('.').first()) }
-        channels = properties("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
+        channels = providers.gradleProperty("pluginVersion").map {
+            Regex(".+-(\\[a-zA-Z]+).*")
+                .find(it)
+                ?.groupValues
+                ?.getOrNull(1)
+                ?: "default"
+        }.map { listOf(it) }
+    }
+
+    // verifyPlugin {
+    //     ides {
+    // //         ides(providers.gradleProperty("pluginVerifierIdeVersions").map { it.split(',') }.getOrElse(emptyList()))
+    // //         recommended()
+    // //         // channels = listOf(ProductRelease.Channel.RELEASE)
+    //
+    // // try
+    // //         select {
+    // //             types = listOf(IntelliJPlatformType.IntellijIdeaCommunity)
+    // //             channels = listOf(ProductRelease.Channel.RELEASE)
+    // //             sinceBuild = "223"
+    // //             untilBuild = "241.*"
+    // //         }
+    //    }
+    // }
+}
+
+// Read more: https://github.com/JetBrains/gradle-changelog-plugin
+changelog {
+    version = providers.gradleProperty("pluginVersion")
+    path = "${rootProject.projectDir}/CHANGELOG.md"
+    header = provider { "[${version.get()}] - ${date()}" }
+    itemPrefix = "-"
+    keepUnreleasedSection = true
+    unreleasedTerm = "[Unreleased]"
+    groups = emptyList()
+    // groups.set(listOf("Added", "Changed", "Deprecated", "Removed", "Fixed", "Security"))
+}
+
+// Java 11 compat started in 2020.3
+// Java 17 compat started in 2021.3
+// Java 21 compat started in 2024.2
+val jvmLanguageLevel = 17
+
+kotlin {
+    jvmToolchain(jvmLanguageLevel)
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.release.set(jvmLanguageLevel)
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "$jvmLanguageLevel"
+
+            // Match the lowest supported version for this platform
+            // See https://plugins.jetbrains.com/docs/intellij/kotlin.html#kotlin-standard-library
+            apiVersion = "1.8"
+            languageVersion = "1.8"
+
+            // Generates default method in Kotlin interfaces to be usable from Java
+            // https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.jvm/-jvm-default/
+            freeCompilerArgs += "-Xjvm-default=all"
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+        // needed for com.intellij.testFramework.UsefulTestCase.DELETE_ON_EXIT_HOOK_CLASS
+        jvmArgs("--add-opens", "java.base/java.io=ALL-UNNAMED")
+    }
+
+
+    printProductsReleases {
+        channels = listOf(ProductRelease.Channel.EAP)
+        types = listOf(IntelliJPlatformType.IntellijIdeaCommunity)
+        untilBuild = provider { null }
+
+        doLast {
+            val latestEap = productsReleases.get().max()
+            println("latest eap " + latestEap)
+        }
+    }
+
+    // runPluginVerifier {
+    //     ideVersions = properties("pluginVerifierIdeVersions").split(',').map(String::trim).filter(String::isNotEmpty)
+    // }
+
+    runIde {
+        // dependsOn(processResources)
+        systemProperty("idea.is.internal", "true");
+    }
+
+    // val runIdeUltimate by registering(CustomRunIdeTask::class) {
+    //     type = IntelliJPlatformType.IntellijIdeaUltimate
+    //     version = providers.gradleProperty("intellij.version")
+    //
+    //     applySystemProperties()
+    // }
+
+    // Latest available EAP release
+    // https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-faq.html#how-to-check-the-latest-available-eap-release
+
+
+    publishPlugin {
+        dependsOn(patchChangelog)
     }
 }
 
-val uiTestTask = tasks.register<Test>("uiTest") {
-    useJUnitPlatform()
-    testLogging { exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL }
-    include("network/radicle/jetbrains/radiclejetbrainsplugin/remoterobot/**")
-    exclude("network/radicle/jetbrains/radiclejetbrainsplugin/remoterobot/endToEnd/**")
-}
-
-val e2eTestTask = tasks.register<Test>("endToEndTests") {
-    useJUnitPlatform()
-    testLogging { exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL }
-    include("network/radicle/jetbrains/radiclejetbrainsplugin/remoterobot/endToEnd/**")
-}
