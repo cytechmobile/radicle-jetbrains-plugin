3270c8f652bd54c39a9c77fb7e7742d966f374b5
stelios maurommatakis <steliosmavr@cytech.gr>
2024-05-22 12:23:31 +0300

    migrate
    Signed-off-by: stelios maurommatakis <steliosmavr@cytech.gr>

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ plugins {
     id("java")
     checkstyle
     alias(libs.plugins.kotlin) // Kotlin support
-    id("org.jetbrains.intellij.platform") version "2.0.0-beta2"
-    id("org.jetbrains.intellij.platform.migration") version "2.0.0-beta2"
+    alias(libs.plugins.intellijPlatformPlugin)
+    alias(libs.plugins.intellijPlatformMigration)
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
 }
 
@@ -29,6 +29,7 @@ repositories {
     }
 }
 
+var remoteRobotVersion: String = libs.versions.remoteRobot.get();
 dependencies {
     intellijPlatform {
         create(
@@ -36,13 +37,12 @@ dependencies {
             providers.gradleProperty("platformVersion")
         )
         bundledPlugins(providers.gradleProperty("platformPlugins").map { it.split(',') }.getOrElse(emptyList()))
-         instrumentationTools()
-         pluginVerifier()
-        // zipSigner()
+        instrumentationTools()
+        pluginVerifier()
+        //zipSigner()
     }
-
-    // testImplementation("com.intellij.remoterobot:remote-robot:$remoteRobotVersion")
-    // testImplementation("com.intellij.remoterobot:remote-fixtures:$remoteRobotVersion")
+    testImplementation("com.intellij.remoterobot:remote-robot:$remoteRobotVersion")
+    testImplementation("com.intellij.remoterobot:remote-fixtures:$remoteRobotVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:${libs.versions.junitJupiter.get()}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${libs.versions.junitJupiter.get()}")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:${libs.versions.junitJupiter.get()}")
@@ -67,40 +67,58 @@ intellijPlatform {
         id = providers.gradleProperty("pluginId")
         name = providers.gradleProperty("pluginName")
         version = providers.gradleProperty("pluginVersion")
+        description = providers.fileContents(layout.projectDirectory.file("./README.md")).asText.map {
+            it.lines().run {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
 
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end))
+            }.joinToString("\n")
+        }.map {
+            markdownToHTML(it)
+        }
 
         // Get the latest available change notes from the changelog file
-        changeNotes.set(provider {
-            changelog.renderItem(
-                changelog.getLatest(),
-                Changelog.OutputType.HTML
-            )
-        })
+        val changelog = project.changelog
+        changeNotes = providers.gradleProperty("pluginVersion").map { pluginVersion ->
+            with(changelog) {
+                renderItem(
+                    (getOrNull(pluginVersion) ?: getUnreleased())
+                        .withHeader(false)
+                        .withEmptySections(false),
+                    Changelog.OutputType.HTML,
+                )
+            }
+        }
 
+        val _untilBuild = if (providers.gradleProperty("pluginUntilBuild").get().isNotEmpty())
+            providers.gradleProperty("pluginUntilBuild") else provider { null }
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
-            untilBuild = provider { null } // removes until-build in plugin.xml
+            untilBuild = _untilBuild
         }
 
-        vendor {
-            name = providers.gradleProperty("pluginVendor")
-            url = providers.gradleProperty("pluginVendorUrl")
-        }
+        //vendor {
+        //    name = providers.gradleProperty("pluginVendor")
+        //    url = providers.gradleProperty("pluginVendorUrl")
+        //}
     }
 
     publishing {
         token = providers.environmentVariable("PUBLISH_TOKEN")
-
         // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels = providers.gradleProperty("pluginVersion").map {
-            Regex(".+-(\\[a-zA-Z]+).*")
-                .find(it)
-                ?.groupValues
-                ?.getOrNull(1)
-                ?: "default"
-        }.map { listOf(it) }
+        channels = providers.gradleProperty("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
+    }
+
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
     }
 
     verifyPlugin {
@@ -112,24 +130,12 @@ intellijPlatform {
             select {
                 types = listOf(IntelliJPlatformType.IntellijIdeaCommunity)
                 channels = listOf(ProductRelease.Channel.RELEASE, ProductRelease.Channel.RC)
-                sinceBuild = "242.*"
-                untilBuild = "242.*"
+                sinceBuild = "223"
+                untilBuild = "241.*"
             }
         }
     }
-
-}
-
-// Read more: https://github.com/JetBrains/gradle-changelog-plugin
-changelog {
-    version = providers.gradleProperty("pluginVersion")
-    path = "${rootProject.projectDir}/CHANGELOG.md"
-    header = provider { "[${version.get()}] - ${date()}" }
-    itemPrefix = "-"
-    keepUnreleasedSection = true
-    unreleasedTerm = "[Unreleased]"
-    groups = emptyList()
-    // groups.set(listOf("Added", "Changed", "Deprecated", "Removed", "Fixed", "Security"))
+    buildSearchableOptions = false
 }
 
 // Java 11 compat started in 2020.3
@@ -167,15 +173,13 @@ tasks {
         jvmArgs("--add-opens", "java.base/java.io=ALL-UNNAMED")
     }
 
-
     printProductsReleases {
         channels = listOf(ProductRelease.Channel.EAP)
         types = listOf(IntelliJPlatformType.IntellijIdeaCommunity)
         untilBuild = provider { null }
 
         doLast {
-            val latestEap = productsReleases.get().max()
-            println("latest eap " + latestEap)
+            productsReleases.get().max()
         }
     }
 
@@ -187,17 +191,6 @@ tasks {
         // dependsOn(processResources)
         systemProperty("idea.is.internal", "true");
     }
-
-    // val runIdeUltimate by registering(CustomRunIdeTask::class) {
-    //     type = IntelliJPlatformType.IntellijIdeaUltimate
-    //     version = providers.gradleProperty("intellij.version")
-    //
-    //     applySystemProperties()
-    // }
-
-    // Latest available EAP release
-    // https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-faq.html#how-to-check-the-latest-available-eap-release
-
 
     publishPlugin {
         dependsOn(patchChangelog)
